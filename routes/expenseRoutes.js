6d81699c973285b7289b3344b76b2308fea4cf6f
Subhodeep Basak <85038045+Subhodeep014@users.noreply.github.com>
2024-05-11 00:49:14 +0530

    Update expenseRoutes.js

--- routes/expenseRoutes.js
+++ routes/expenseRoutes.js
@@ -1,14 +1,9 @@
 const express = require("express");
 const router = express.Router()
-const cors = require("cors");
+
 const { addExpense, getExpenses, deleteExpense } = require("../controllers/expenseController");
 
-router.use(
-    cors({
-        credentials: true,
-        origin: 'https://expensereactapp.netlify.app/api'
-    })
-)
+
 
 router.post('/addexpense', addExpense)
 router.get('/getexpenses', getExpenses)
