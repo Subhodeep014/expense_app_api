5849e854948ad1ffdb679ad1068d663b6a386786
Subhodeep Basak <85038045+Subhodeep014@users.noreply.github.com>
2024-05-11 00:48:59 +0530

    Update budgetRoute.js

--- routes/budgetRoute.js
+++ routes/budgetRoute.js
@@ -1,15 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-const cors = require('cors');
+
 const { createBudget, getbudgets, getBudgetsById, deleteBudget } = require('../controllers/budgetController');
 
-router.use(
-    cors({
-        credentials: true,
-        origin : 'https://expensereactapp.netlify.app/api'
-    })
-)
+
 router.post('/addbudget', createBudget)
 router.get('/getbudgets', getbudgets)
 router.get('/budget/:id', getBudgetsById)
