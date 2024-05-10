0001b5f0eefea9c55553e381bba5b320da5b0d00
Subhodeep Basak <85038045+Subhodeep014@users.noreply.github.com>
2024-05-11 00:42:56 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,17 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const cors = require('cors');
+
 const { test, registerUser, loginUser, getProfile, logoutUser } = require('../controllers/authController');
 
-const corsOptions = {
-  origin: 'https://expensereactapp.netlify.app', // Replace with your React app's URL
-  optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true // Allow sending cookies across domains
-}
 
-// Apply the CORS middleware to all routes in your router
-router.use(cors(corsOptions));
 
 router.post('/signup', registerUser)
 router.post('/signin', loginUser)
