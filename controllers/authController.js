87370b94e041bdab422b0eccea7f4ee031014961
Subhodeep Basak <85038045+Subhodeep014@users.noreply.github.com>
2024-05-12 23:53:12 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -88,7 +88,7 @@ const getProfile = (req,res)=>{
 const logoutUser = (req,res)=>{
     try{
         res.clearCookie('token');
-        res.header('Access-Control-Allow-Origin', 'https://subhodeep014.github.io/react_expense_app_deploy');
+        res.header('Access-Control-Allow-Origin', 'https://subhodeep014.github.io');
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         
