3f897fbfdb7b45f93920750a6463821b59f3e3bd
Subhodeep Basak <85038045+Subhodeep014@users.noreply.github.com>
2024-05-12 23:34:34 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -88,8 +88,12 @@ const getProfile = (req,res)=>{
 const logoutUser = (req,res)=>{
     try{
         res.clearCookie('token');
-        console.log("hit logout route")
-        res.header('Access-Control-Allow-Origin', '*'); // Set appropriate origin instead of *
+        res.header('Access-Control-Allow-Origin', 'https://subhodeep014.github.io');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        
+        res.status(200).json({ message: "Successfully logged out" });
+
     }catch(error){
         res.json({
             error: "Error while signing out!"
